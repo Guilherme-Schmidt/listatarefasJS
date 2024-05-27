653cfec91650253c03ab36196d621fd7a266f156
Guilherme Schmidt <guischmidt201@gmail.com>
2024-05-27 15:36:02 -0300

    add: itens 4 e 5

--- script.js
+++ script.js
@@ -1,69 +1,109 @@
 function addElemento() {
   let li = document.createElement("li");
-  let inputValue = document.getElementById("tarefa").value.toUpperCase();
+  let inputValue = document.getElementById("tarefa").value.trim().toUpperCase();
+  let dataValue = document.getElementById("data").value;
+  let categoriaValue = document.getElementById("categoria").value;
 
-  const data = new Date() 
-  let dia = data.getDate()
-  let mes = data.getMonth() + 1
-  let ano = data.getFullYear()
+  // Check if input is just the date (no task description)
+  if (!inputValue || !dataValue) {
+    alert("Por favor, preencha a descrição da tarefa e selecione a data.");
+    return;
+  }
 
-  let formatedCurrentDate = dia+'/'+ mes+'/'+ano+' '
-  inputValue = formatedCurrentDate + inputValue
+  let formattedDate = new Date(dataValue).toLocaleDateString();
+  inputValue = `${formattedDate} - ${inputValue} [${categoriaValue}]`;
 
   let t = document.createTextNode(inputValue);
   li.appendChild(t);
 
-  if (inputValue === formatedCurrentDate) {
-  alert("Você precisa descrever a tarefa");
-  } else {
-    document.getElementById("itemLista").appendChild(li);
-  }
+  document.getElementById("itemLista").appendChild(li);
 
   document.getElementById("tarefa").value = "";
-  
+  document.getElementById("data").value = "";
+  document.getElementById("categoria").value = "Trabalho";
+
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
-  
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-  
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      let div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+
+  let editSpan = document.createElement("SPAN");
+  let editTxt = document.createTextNode("✎");
+  editSpan.className = "edit";
+  editSpan.appendChild(editTxt);
+  li.appendChild(editSpan);
+
+  span.onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+
+  editSpan.onclick = function() {
+    let li = this.parentElement;
+    let taskText = li.childNodes[0].nodeValue;
+    let taskCategory = taskText.match(/\[(.*?)\]$/)[1];
+    let taskDate = taskText.split(" - ")[0];
+    let taskDescription = taskText.split(" - ")[1].replace(/\[.*?\]$/, '').trim();
+    document.getElementById("data").value = taskDate;
+    document.getElementById("tarefa").value = taskDescription;
+    document.getElementById("categoria").value = taskCategory;
+    li.style.display = "none";
+  };
 }
 
+// Add close buttons and edit buttons to existing list items
 let myNodelist = document.getElementsByTagName("li");
-
 for (let i = 0; i < myNodelist.length; i++) {
-  let span = document.createElement("span");
-  let txt = document.createTextNode("\u00D7"); //caracter x
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
+
+  let editSpan = document.createElement("SPAN");
+  let editTxt = document.createTextNode("✎");
+  editSpan.className = "edit";
+  editSpan.appendChild(editTxt);
+  myNodelist[i].appendChild(editSpan);
 }
 
+// Add event listeners to close buttons and edit buttons
 let close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     let div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
-let list = document.querySelector('ul');
-list.addEventListener('click', 
-  function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    }
-  }
-, false);
+let edit = document.getElementsByClassName("edit");
+for (let i = 0; i < edit.length; i++) {
+  edit[i].onclick = function () {
+    let li = this.parentElement;
+    let taskText = li.childNodes[0].nodeValue;
+    let taskCategory = taskText.match(/\[(.*?)\]$/)[1];
+    let taskDate = taskText.split(" - ")[0];
+    let taskDescription = taskText.split(" - ")[1].replace(/\[.*?\]$/, '').trim();
+    document.getElementById("data").value = taskDate;
+    document.getElementById("tarefa").value = taskDescription;
+    document.getElementById("categoria").value = taskCategory;
+    li.style.display = "none";
+  };
+}
 
-function clearList(){
+// Toggle checked class on and off when clicking list items
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+function clearList() {
   document.getElementById("itemLista").innerHTML = "";
-  
 }
